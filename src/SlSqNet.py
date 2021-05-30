40050e9546c485aa36c4c67b9fe4f40c8eb7bd42
Kudryavtsev <vadkudr@vadkudr.org>
2021-05-30 15:12:07 +0900

    NNet state-value and action-value separated

--- src/SlSqNet.py
+++ src/SlSqNet.py
@@ -3,8 +3,22 @@
 from tensorflow.keras.layers import Input, Dense
 from tensorflow.keras.models import Model
 
-def SlSqNet(input_shape,
-            L1_num,L2_num,A1_num,V1_num,
+def SlSq_val(input_shape,
+            L1_num,L2_num,V1_num) :
+        
+    inputs=Input(shape=input_shape)
+    x=Dense(L1_num,activation='relu')(inputs)
+    x=Dense(L2_num,activation='relu')(x)
+    
+    v=Dense(V1_num,activation='relu')(x)
+    v=Dense(1,name='state')(v)
+
+    model=Model(inputs,v,name='SlSq_val')
+
+    return model
+
+def SlSq_act(input_shape,
+            L1_num,L2_num,A1_num,
             Aout_num) :
         
     inputs=Input(shape=input_shape)
@@ -12,11 +26,8 @@ def SlSqNet(input_shape,
     x=Dense(L2_num,activation='relu')(x)
     
     a=Dense(A1_num,activation='relu')(x)
-    a=Dense(Aout_num,name='action')(a)
+    a=Dense(Aout_num,activation='softmax',name='action')(a)
 
-    v=Dense(V1_num,activation='relu')(x)
-    v=Dense(1,name='state')(v)
-
-    model=Model(inputs,[a,v],name='SlSqNet')
+    model=Model(inputs,a,name='SlSq_act')
 
     return model
--- src/TD0_train.py
+++ src/TD0_train.py
@@ -4,13 +4,10 @@ import numpy as np
 from tensorflow import keras
 from tensorflow.keras.models import Sequential, clone_model
 from tensorflow.keras.layers import Dense
-# import collections
+from SlSqNet import SlSq_val, SlSq_act
 import random
 
 from slidesquare_env import SquareEnv
-
-# SN=2
-# HLN=8
 
 SN=3
 HLN=32
@@ -23,22 +20,19 @@ gamma=0.9
 
 square_env=SquareEnv(SN)
 
-# model
-from SlSqNet import SlSqNet
-model_val=SlSqNet(int(np.prod(square_env.encoded_shape)),
-                L1_num,L2_num,A1_num,V1_num,
+# model for state value
+model_val=SlSq_val(int(np.prod(square_env.encoded_shape)),
+                L1_num,L2_num,V1_num)
+loss_val = "mean_squared_error"
+model_val.compile(loss=loss_val, optimizer=keras.optimizers.RMSprop(lr=1e-3))
+
+# model for action value
+model_act=SlSq_act(int(np.prod(square_env.encoded_shape)),
+                L1_num,L2_num,A1_num,
                 len(square_env.action_enum))
+loss_act = "categorical_crossentropy"
+model_act.compile(loss=loss_act, optimizer=keras.optimizers.RMSprop(lr=1e-3))
 
-losses = {
-	"action": "categorical_crossentropy",
-	"state": "mean_squared_error",
-}
-lossWeights = {"action": 3.0, "state": 1.0}
-model_val.compile(loss=losses,
-              optimizer=keras.optimizers.RMSprop(lr=1e-4))
-
-model_act=clone_model(model_val)
-model_act.set_weights(model_val.get_weights())
 
 def encode_states(env, states):
     assert isinstance(env, SquareEnv)
@@ -92,7 +86,59 @@ def make_pilot_data_buffer(env, buf_size, scramble_depth):
         result.append((enc_s, depth, env.is_goal(s), enc_states, goals))
     return result
 
-def prepare_batch(env, model_val, model_act, buf_size, scramble_depth):
+# def prepare_batch(env, model_val, model_act, buf_size, scramble_depth):
+#     """
+#     Sample batch of given size from scramble buffer produced by make_scramble_buffer
+#     :param scramble_buffer: scramble buffer
+#     :param model: network to use to calculate targets
+#     :param batch_size: size of batch to generate
+#     :param value_targets: targets
+#     :return: tensors
+#     """
+#     data = make_pilot_data_buffer(env, buf_size, scramble_depth)
+#     states, depths, is_goals, explored_states, explored_goals = zip(*data)
+
+#     # handle visited states
+#     states = np.stack(states)
+#     shape = states.shape
+#     [act_t,_] = model_act.predict(np.reshape(states,(shape[0], shape[2]*shape[3])))
+#     act_t=np.argmax(act_t,axis=-1)
+
+#     # handle explored states
+#     explored_states = np.stack(explored_states)
+#     shape = explored_states.shape
+    
+#     [_,value_t] = model_val.predict(np.reshape(explored_states,(shape[0]*shape[1], shape[2]*shape[3])))
+#     value_t = value_t.reshape(shape[0], shape[1])
+#     value_train_t=value_t[range(shape[0]),act_t]
+#     R=-1
+#     value_train_t = value_train_t * gamma + R
+#     value_train_t=np.reshape(value_train_t,(shape[0],1))
+
+#     # force goal states to get 0
+#     explored_goals=np.stack(explored_goals)
+#     explored_goals=explored_goals[range(shape[0]),act_t]
+#     for i in range(len(explored_goals)) :
+#         if explored_goals[i]==True :
+#            value_train_t[i]=R
+#         if is_goals[i]==True :
+#            value_train_t[i]=0
+
+#     max_act_t = np.argmax(value_t, axis=1)
+#     # max_val_t=value_t[range(shape[0]),max_act_t]
+    
+#     # goal_indices = np.nonzero(is_goals)
+#     # max_val_t[goal_indices] = 0.0
+#     # max_act_t[goal_indices] = 0
+
+#     enc_input = np.reshape(np.stack(states),(shape[0], shape[2]*shape[3]))
+#     # weights = np.reshape(1/np.asarray(depths),(shape[0],1))
+#     max_act_t=np.eye(shape[1])[max_act_t]
+#     # max_val_t=np.reshape(max_val_t,(shape[0],1))
+    
+#     return enc_input, max_act_t, value_train_t
+
+def prepare_state_batch(env, model_val, model_act, buf_size, scramble_depth):
     """
     Sample batch of given size from scramble buffer produced by make_scramble_buffer
     :param scramble_buffer: scramble buffer
@@ -107,14 +153,14 @@ def prepare_batch(env, model_val, model_act, buf_size, scramble_depth):
     # handle visited states
     states = np.stack(states)
     shape = states.shape
-    [act_t,_] = model_act.predict(np.reshape(states,(shape[0], shape[2]*shape[3])))
+    act_t = model_act.predict(np.reshape(states,(shape[0], shape[2]*shape[3])))
     act_t=np.argmax(act_t,axis=-1)
 
     # handle explored states
     explored_states = np.stack(explored_states)
     shape = explored_states.shape
     
-    [_,value_t] = model_val.predict(np.reshape(explored_states,(shape[0]*shape[1], shape[2]*shape[3])))
+    value_t = model_val.predict(np.reshape(explored_states,(shape[0]*shape[1], shape[2]*shape[3])))
     value_t = value_t.reshape(shape[0], shape[1])
     value_train_t=value_t[range(shape[0]),act_t]
     R=-1
@@ -130,20 +176,38 @@ def prepare_batch(env, model_val, model_act, buf_size, scramble_depth):
         if is_goals[i]==True :
            value_train_t[i]=0
 
-    max_act_t = np.argmax(value_t, axis=1)
-    # max_val_t=value_t[range(shape[0]),max_act_t]
+    enc_input = np.reshape(np.stack(states),(shape[0], shape[2]*shape[3]))
     
-    # goal_indices = np.nonzero(is_goals)
-    # max_val_t[goal_indices] = 0.0
-    # max_act_t[goal_indices] = 0
+    return enc_input, value_train_t
+
+def prepare_action_batch(env, model_val, model_act, buf_size, scramble_depth):
+    """
+    Sample batch of given size from scramble buffer produced by make_scramble_buffer
+    :param scramble_buffer: scramble buffer
+    :param model: network to use to calculate targets
+    :param batch_size: size of batch to generate
+    :param value_targets: targets
+    :return: tensors
+    """
+    data = make_pilot_data_buffer(env, buf_size, scramble_depth)
+    states, depths, is_goals, explored_states, explored_goals = zip(*data)
+
+    # handle explored states
+    explored_states = np.stack(explored_states)
+    shape = explored_states.shape
+    
+    value_t = model_val.predict(np.reshape(explored_states,(shape[0]*shape[1], shape[2]*shape[3])))
+    value_t = value_t.reshape(shape[0], shape[1])
+    R=-1
+    value_t = value_t * gamma + R
+    value_t=np.reshape(value_t,(shape[0],shape[1]))
+
+    max_act_t = keras.utils.to_categorical(np.argmax(value_t, axis=-1))
 
     enc_input = np.reshape(np.stack(states),(shape[0], shape[2]*shape[3]))
-    # weights = np.reshape(1/np.asarray(depths),(shape[0],1))
-    max_act_t=np.eye(shape[1])[max_act_t]
-    # max_val_t=np.reshape(max_val_t,(shape[0],1))
+    # max_act_t=np.eye(shape[1])[max_act_t]
     
-    return enc_input, max_act_t, value_train_t
-
+    return enc_input, max_act_t
 
 def store_data(env,model) :
     from gen_all_pos import gen_all_pos
@@ -155,7 +219,7 @@ def store_data(env,model) :
     for i in range(len(all_pos_list)) :
         encs[i,:]=np.ravel(env.encode_inplace(env.ConvertToState(all_pos_list[i].astype(int))))
 
-    [_,v_list]=model.predict(encs)
+    v_list=model.predict(encs)
 
     fid=open("sl_"+str(SN)+".txt","w")
     np.savetxt(fid,v_list)
@@ -168,24 +232,20 @@ train_batch_size=250
 train_buf_size=1000
 train_scramble_depth=10
 
-# print("Generate scramble buffer...")
-# scramble_buf = collections.deque(maxlen=scramble_buffer_batches*train_batch_size)
-# scramble_buf.extend(make_scramble_buffer(square_env, train_batch_size*2, train_scramble_depth))
-
 cnt=0
 while 1 :
-    # x_t, weights_t, y_policy_t, y_value_t = sample_batch(scramble_buf, model, train_batch_size)
-    x_t, y_policy_t, y_value_t = prepare_batch(square_env, model_val,model_act,train_buf_size,train_scramble_depth)
-    print("Pushed new data in train buffer")
+    for i in range(10) :
+        x_t, y_value_t = prepare_state_batch(square_env, model_val,model_act,train_buf_size,train_scramble_depth)
+        print("i = "+str(i)+". Pushed new data in train buffer")
+        model_val.fit(x_t,y_value_t,batch_size=train_batch_size,epochs=10,verbose=1)
 
-    model_val.fit(x_t,[y_policy_t, y_value_t],batch_size=train_batch_size,epochs=200,verbose=1)
+    for k in range(10) :
+        x_t, y_policy_t = prepare_action_batch(square_env, model_val,model_act,train_buf_size,train_scramble_depth)
+        print("k = "+str(k)+". Pushed new data in train buffer")
+        model_act.fit(x_t,y_policy_t,batch_size=train_batch_size,epochs=10,verbose=1)
 
     cnt+=1
     print("==> cnt = "+str(cnt))
-
-    if cnt % 10 == 0 :
-        model_act.set_weights(model_val.get_weights())
-        print("model_act updated")
 
     if cnt % 10 ==0 :
         store_data(square_env,model_val)
